e22a1c13d41272d0f2f0ed8119c75257fa2b7385
Patrick Dobler <patrick.dobler@gmail.com>
2014-06-29 11:06:55 +0200

    added angular + grunt + bower from yeoman

---
+++ kitchenbattle-web/src/main/resources/static/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name kitchenbattleWebApp
+ * @description
+ * # kitchenbattleWebApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('kitchenbattleWebApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ kitchenbattle-web/src/main/resources/static/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name kitchenbattleWebApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the kitchenbattleWebApp
+ */
+angular.module('kitchenbattleWebApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
