e4ef890f2bad0dc43965fb4cbf0f909076aaad02
Patrick Dobler <patrick.dobler@gmail.com>
2014-09-07 17:54:00 +0200

    updated to spring boot 1.1.6 + added favicions for apple devices

--- kitchenbattle-web/src/main/java/ch/adrianos/apps/kitchenbattle/config/CustomSecurityConfig.java
+++ kitchenbattle-web/src/main/java/ch/adrianos/apps/kitchenbattle/config/CustomSecurityConfig.java
@@ -2,7 +2,9 @@ package ch.adrianos.apps.kitchenbattle.config;
 
 import com.google.common.base.Preconditions;
 import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.Ordered;
@@ -11,6 +13,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PropertiesLoaderUtils;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configurers.GlobalAuthenticationConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -45,21 +50,32 @@ public class CustomSecurityConfig implements InitializingBean {
         }
     }
 
-//    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
-//    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
-//
-//        @Autowired
-//        private SecurityProperties security;
-//
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.authorizeRequests().anyRequest().fullyAuthenticated().and().formLogin().permitAll();
-//            http.csrf().disable();
-//        }
-//    }
-//
-//    @Bean
-//    public ApplicationSecurity applicationSecurity() {
-//        return new ApplicationSecurity();
-//    }
+    @Bean
+    public ApplicationSecurity applicationSecurity() {
+        return new ApplicationSecurity();
+    }
+
+    @Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
+    protected static class ApplicationSecurity extends WebSecurityConfigurerAdapter {
+
+        @Autowired
+        private SecurityProperties security;
+
+        @Override
+        public void configure(WebSecurity webSecurity) throws Exception {
+            webSecurity
+                    .ignoring()
+                    .antMatchers("/images/**")
+                    .antMatchers("/styles/**")
+                    .antMatchers("/vendor/**");
+        }
+
+        //        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.authorizeRequests().anyRequest().fullyAuthenticated()
+                    .and().httpBasic().realmName("KB APP");
+            http.csrf().disable();
+        }
+
+    }
 }
