e79fa9cca6bf9d1357f871700c78f2f1a0f92ea4
Patrick Dobler <patrick.dobler@gmail.com>
2014-09-12 20:40:59 +0200

    delete old files

--- kitchenbattle-web/src/main/resources/static/scripts/app.js
+++ kitchenbattle-web/src/main/resources/static/scripts/app.js
@@ -11,7 +11,6 @@ var app = angular.module('kitchenbattleWebApp', [
     'lr.upload',
     'commonsModule',
     'courseBattleVotingModule',
-    'battleResultModule',
     'adminModule',
     'FBAngular'
 ]);
